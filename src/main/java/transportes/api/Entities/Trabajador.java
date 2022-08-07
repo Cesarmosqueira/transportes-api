6e8b42895da51df68b4b0f0bd3c09f60dea11173
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-08-07 17:17:50 -0500

    asistencia trabajador rel

--- src/main/java/transportes/api/Entities/Trabajador.java
+++ src/main/java/transportes/api/Entities/Trabajador.java
@@ -1,12 +1,16 @@
 package transportes.api.Entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -19,7 +23,7 @@ import lombok.Data;
 public class Trabajador {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+	private Long id;
 
 	@Column(nullable = false, length = 64)
 	private String nombreCompleto;
@@ -88,4 +92,8 @@ public class Trabajador {
 
 	@Column(nullable = false)
 	private int sueldo;
+
+	// Relations
+	@OneToMany(mappedBy = "trabajador", cascade = CascadeType.ALL)
+	private List<TrabajadorAsistencia> asistencia = new ArrayList<>();
 }
--- src/main/java/transportes/api/Entities/TrabajadorAsistencia.java
+++ src/main/java/transportes/api/Entities/TrabajadorAsistencia.java
@@ -4,19 +4,22 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "asistencia_trabajador")
-public class AsistenciaTrabajador {
+@Table(name = "trabajador_asistencia")
+public class TrabajadorAsistencia {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+	private Long id;
 
 	@Temporal(TemporalType.DATE)
 	@Column(nullable = false, unique = true)
@@ -25,4 +28,7 @@ public class AsistenciaTrabajador {
 	@Column(nullable = false, length = 32)
 	private String estado;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "trabajador_id", nullable = false)
+	private Trabajador trabajador;
 }
