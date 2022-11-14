8ff8cc9f774e7a448eb4a7a7989869d6c3557f27
FrankHuarcaya <u201624288@upc.edu.pe>
2022-11-14 12:24:46 -0500

    update relations

--- src/main/java/pe/com/huex/providers/service/resouces/dto/TireReplacementDto.java
+++ src/main/java/pe/com/huex/providers/service/resouces/dto/TireReplacementDto.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class TireReplacementDto {
 
     private Long id;
-    private Provider provider;
+    private Long providerId;
     private Date replacementDate;
     private Integer tireQuantity;
     private Double unitPrice;
