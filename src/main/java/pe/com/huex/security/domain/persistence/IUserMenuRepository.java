7126c15ca4c927de274b2b54816a3520707f98a2
jgala <jgala@banbif.com.pe>
2023-02-15 10:24:36 -0500

    upd tracking

--- src/main/java/pe/com/huex/security/domain/persistence/IUserMenuRepository.java
+++ src/main/java/pe/com/huex/security/domain/persistence/IUserMenuRepository.java
@@ -1,9 +1,11 @@
 package pe.com.huex.security.domain.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import pe.com.huex.security.domain.entities.UserMenu;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface IUserMenuRepository extends JpaRepository<UserMenu, Long> {
@@ -11,6 +13,8 @@ public interface IUserMenuRepository extends JpaRepository<UserMenu, Long> {
     @Query(value = "SELECT * FROM user_menu k WHERE id_user = ?1", nativeQuery = true)
     List<UserMenu> findByIdUser(Long idUser);
 
-    @Query(value = "DELETE user_menu k WHERE id_user = ?1", nativeQuery = true)
+    @Query(value = "DELETE FROM user_menu k WHERE id_user = ?1", nativeQuery = true)
+    @Modifying
+    @Transactional
     int deleteByIdUser(Long idUser);
 }
--- src/main/java/pe/com/huex/services/service/resources/dto/TrackingServiceDto.java
+++ src/main/java/pe/com/huex/services/service/resources/dto/TrackingServiceDto.java
@@ -1,5 +1,6 @@
 package pe.com.huex.services.service.resources.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import pe.com.huex.customer.services.resources.dto.RateDto;
 import pe.com.huex.employees.services.resources.dtos.EmployeeDto;
@@ -22,13 +23,16 @@ public class TrackingServiceDto {
     private String additionalCost;
     private String observations;
     private String guideNumber;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date datePrecharge;
     private String preloadStatus;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date scheduledAppointment;
     private RateDto rate;
     private EmployeeDto driver;
     private EmployeeDto copilot;
     private EmployeeDto stevedore;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date dateTimeCompletion;
     private Long weightLoad;
     private BigDecimal moneyDelivered;
